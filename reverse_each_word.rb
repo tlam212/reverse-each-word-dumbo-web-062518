5c0a784538eead42591d0cdd2aa493ed171b157b
Toni Lamont <tlam212@gmail.com>
2018-06-18 16:38:52 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   new_array = sentence1.split(" ")
-  reverse_array = new_array.each {|sentence| sentence.reverse!}
+  reverse_array = new_array.each {|sentence| sentence.reverse!}.join(" ")
   
   
 end
